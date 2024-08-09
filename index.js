6b4c0a95145553d6a5c69945b69b03e2a3d38b71
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-09 19:13:01 +0200

    Alternative file export and import

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { tshepo, dumi } = require("./names");
+const { tshepo, dumi } = require("./names"); // DESTRUCTURING.
 const sayHi = require("./sayHi");
+const { brother, items } = require("./alternative-flavor"); // PREFER DESTRUCTURING.
 
 sayHi(dumi);
 sayHi(tshepo);
 sayHi("Mama");
+
+console.log(items, brother);
