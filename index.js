3c980e5774b16b8dad97fb07c5c9d93223e3d362
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-09 18:48:43 +0200

    Encapsulated code(modules exports)

---
+++ index.js
@@ -0,0 +1,6 @@
+const { tshepo, dumi } = require("./names");
+const sayHi = require("./sayHi");
+
+sayHi(dumi);
+sayHi(tshepo);
+sayHi("Mama");
---
+++ sayHi.js
@@ -0,0 +1,5 @@
+module.exports = (name) => {
+  console.log(`Hello ${name}`);
+};
+
+// module.exports = sayHi;
