f5a15a6c5b30e21f31b78b212822244670499fc4
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-10 05:17:36 +0200

    Add npm.js file and install nodemon

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-/*
-! npm (node packaging manager)
-* local dependencies - use it only in this partiucular project (npm i <packageName>) OR
-* global dependencies - use it in any project (npm i -g <packageName>)
-? package.json can be regarded as manifest file that stores important info about project (npm init -y) 
-*/
-const _ = require("lodash");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
