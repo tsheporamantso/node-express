05b2e8a96b1ffce6b2f441f4622be7f68749e45a
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-11 14:23:01 +0200

    Event emitter noted recap

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,9 +1,16 @@
 const EventEmitter = require("events");
 
+/*
+! Two methods. the on() will listen to event and emit() will emit the event(Watch video for clarity!!)
+? WHEN YOU EMIT THE EVENT THE FIRST ARGUMENT IS THE STRING THAT YOU LISTENING TOO.
+? You can have as many on events listening to the same emit.
+? The order matters, YOU FIRST HAVE TO LISTEN AND THAN EMIT
+*/
+
 const customeEmitter = new EventEmitter();
 
 customeEmitter.on("response", (name, id) => {
-  console.log(`data received from user ${name} with id:${id}`);
+  console.log(`Data received from user ${name} with id:${id}`);
 });
 
 customeEmitter.on("response", () => {
@@ -11,3 +18,4 @@ customeEmitter.on("response", () => {
 });
 
 customeEmitter.emit("response", "Tshepo", 668);
+customeEmitter.emit("response", "Dumisani", 2157);
